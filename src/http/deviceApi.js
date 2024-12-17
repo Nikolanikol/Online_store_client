f32be185d2c8efcfeb109878162271cdb130ec4d
Nikolai <krezi3927@gmail.com>
2024-12-18 00:30:39 +0900

    console

--- src/http/deviceApi.js
+++ src/http/deviceApi.js
@@ -4,22 +4,27 @@ import { jwtDecode } from "jwt-decode";
 
 export const createType = async(type) =>{
     const {data} = await $authHost.post('api/type', type)
+    console.log(data)
     return data
 }
 
 export const fetchTypes = async() =>{
     const {data} = await $host.get('api/type')   
-  
+    console.log(data)
+
     return data
 }
 
 export const createBrand = async(brand) =>{
     const {data} = await $authHost.post('api/brand', brand)
+    console.log(data)
+
     return data
 }
 export const fetchBrand = async() =>{
     const {data} = await $host.get('api/brand')   
- 
+    console.log(data)
+
     return data
 }
 
